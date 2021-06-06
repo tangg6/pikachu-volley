750ba7e6c8796882ed45806d8e9c58e51b5991be
Tanabut <tangg6@outlook.com>
2021-06-06 20:30:20 +0700

    almost done (about 90%)

--- client.py
+++ client.py
@@ -9,86 +9,114 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = '1000,200'
 
 import threading
 
+# ----------------------- Function to create thread -----------------
+
 def create_thread(target):
     thread = threading.Thread(target=target)
     thread.daemon = True # Use deamon thread
     thread.start()
 
-#------------------------ Open server ------------------------------
+#------------------------ Connect to server  ------------------------------
 
 import socket
+
 HOST = '127.0.0.1'
 PORT = 62107
-connection_established = False
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+server.connect((HOST,PORT))         # Connect
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect((HOST,PORT)) # เชื่อมต่อ
 
-#------------------------- Recieve data from client --------------------------------
+#------------------------- Recieve data from server --------------------------------
 
 def recieve_data():
-    global x1,y1,pos_x,pos_y
+    global x1,y1,pos_x,pos_y,score_player1,score_player2
     while True:
         data = server.recv(1024).decode()
         data = data.split('-')
-        #print(data)
         x1, y1 = float(data[0]), float(data[1])
-        pos_x, pos_y = float(data[2]), float(data[3])
+        if data[2] != '':                                   # To fix bug 
+            pos_x, pos_y = float(data[2]), float(data[3])
+            score_player1,score_player2 = data[4], data[5]
+
+#------------------------ Another thread doing about server ------------------------------
 
 create_thread(recieve_data)
 
 #------------------------- Create a display of game ---------------------------------
-size_game = (800,600)
+
+size_game = (g_width,g_height) = (800,600)
 surface = pygame.display.set_mode(size_game)
-pygame.display.set_caption("Ball") #caption
-# icon = pygame.image.load(os.path.join('games.png'))
-# pygame.display.set_icon (icon)
+pygame.display.set_caption("Pikachu Volley") 
+icon = pygame.image.load(os.path.join('src','icon.png'))
+pygame.display.set_icon (icon)
 clock = pygame.time.Clock() 
 
-x1, y1 = 375, 490
+#------------------------ Declare values before recieve from client ----------------
 
-pos_x, pos_y = 40, 40
-block_size = 60
+x1, y1 = 50, 490                        # Declare position of player 1 before recieve position from server
+pos_x, pos_y = 40, 40                   # Declare position of ball before recieve position from server
+block_size = 60         
+score_player1,score_player2 = '0','0'   # Declare score before recieve position from server
+
+
+#----------------------------------- Main -------------------------------------------
 
 def start_the_game():
-    # Main game
-
     running = True
-    x2, y2 = 675, 500
 
-    move = 20
-    jump = False
-    jump_count = 10
-    grav = 0.6
+    x2, y2 = 675, 500   # Initial position of player 2
 
-    
+    # Detail of player 
+    move = 20               # Speed of player 
+    jump = False            # To check player jumping?
+    jump_count = 10         # State of jumping
+    grav = 0.5              # Gravity
 
+    # Import background in to game
     surface = pygame.display.set_mode(size_game)
-    bg = pygame.image.load("pikachu_background.png")
+    bg = pygame.image.load(os.path.join('src','pikachu_background.png'))
+
+    # Import image of player, ball
+    img_ball = pygame.image.load(os.path.join('src','ball.png'))
+    img_player1 = pygame.image.load(os.path.join('src','pika.png'))
+    img_player2 = pygame.image.load(os.path.join('src','pika2.png'))
+
     
+    #------------------- Game when create server and client connected --------------
     while running:
         
+        # -------------- Condtion before get into the game ------------
+        keyspressed = pygame.key.get_pressed()
+        for event in pygame.event.get():            # User did something 
+            if event.type == pygame.QUIT:           # If user clicked exit
+                running = False                     # End the loop
+
+
+        # ------------------ Make a shape of player --------
         player2 = Rect(x2, y2, 50, 80)
         player1 = Rect(x1, y1, 50, 80)
+
+        # --------------------- Making a ball -----------------------------
         ball = Rect(pos_x,pos_y,block_size,block_size)
-        
+
+        # ---------------------- Input background ----------
         surface.blit(bg, (0, 0))
-        
-        keyspressed = pygame.key.get_pressed()
-        for event in pygame.event.get(): # User did something
-            #print(event)    # Useful debugging tip    
-            if event.type == pygame.QUIT: # If user clicked close
-                running = False # Flag that we are done so we exit this loop
-        if keyspressed[ord("\x1b")]: # Pressing the x Key will quit the game
-                running = False             
-        if keyspressed[ord("a")]:
+
+
+        # -------------- Pressing the ESC Key to quit the game --------
+        if keyspressed[ord("\x1b")]: 
+                running = False        
+
+
+        # ------- Moving player ----------
+        if keyspressed[ord("a")]:                   # Pressing A to move left
             x2 -= move
-        if keyspressed[ord("d")]:
+        if keyspressed[ord("d")]:                   # Pressing D to move right
             x2 += move
-        
-        if not(jump):
-            if keyspressed[ord("w")] or keyspressed[ord(" ")]:
-                jump = True
+        if not(jump):                               # Pressing W or Spacebar to jump
+            if keyspressed[ord("w")] \
+            or keyspressed[ord(" ")]:
+                jump = True                         # If jump = True we will calculate Gravity
         else:
             if jump_count >= -10:
                 neg = 1
@@ -99,36 +127,46 @@ def start_the_game():
             else:
                 jump = False
                 jump_count = 10
-            
+        
+
+        #---------------- Detect player won't out of bound --------------
         if y2 < 0: 
             y2 = 0
         if y2 >= surface.get_height()-110: 
             y2 = surface.get_height()-110
-        if x2 < 400: 
-            x2 = 400
-        if x2 >= surface.get_width()-50: 
-            x2 = surface.get_width()-50   
+        if x2 < 405: 
+            x2 = 405
+        if x2 >= surface.get_width()-70: 
+            x2 = surface.get_width()-70   
         
-        pygame.draw.ellipse(surface, (0,0,0),ball)
-        
-        
-        pygame.draw.rect(surface, (0,0,255), player2)   
+    
 
-        
-        pygame.draw.rect(surface, (204,0,255), player1)
 
+        # ------------------------ Display Objective --------------------------
+        surface.blit(img_ball,ball.topleft)       # Display ball as Pokeball
+        surface.blit(img_player1,player1.topleft) # Draw player1 as pikachu 
+        surface.blit(img_player2,player2.topleft) # Draw player2 as pikachu 
+        
+
+        # ------------------------ Display score ------------------------------
+        font = pygame.font.Font(None, 80)
+        text_score_player1 = font.render(score_player1, False, (255, 0, 0))
+        text_score_player2 = font.render(score_player2, False, (255, 0, 0))      
+        surface.blit(text_score_player1, (60,60))
+        surface.blit(text_score_player2, (740,60))
+
+
+        # ------------------------ Send data to server ------------------------
         send_data = '{}-{}'.format(x2, y2).encode()       # Use format string to enable encode function
         server.send(send_data)
 
-        
+
         pygame.display.update()                         # Actually does the screen update
-        clock.tick(30)                                  # Run the game at 25 frames per second  
+        clock.tick(30)                                  # Run the game at 30 frames per second  
         
+#------------------------ Menu before enter to the game --------------------------
 menu_widgth, menu_height = 400, 250
 menu = pygame_menu.Menu('Pikachu',menu_widgth,menu_height ,theme = pygame_menu.themes.THEME_DARK)
 menu.add.button('Play', start_the_game)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 menu.mainloop(surface)
-
-# Declare grid
-#grid = Grid()
--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 import pygame
-from pygame.draw import circle, rect
 import pygame_menu
 from pygame.locals import *
-#from game import Game
 
 pygame.init()
 
@@ -10,6 +8,8 @@ import os
 os.environ['SDL_VIDEO_WINDOW_POS'] = '200,200'
 
 import threading
+
+# ----------------------- Function to create thread -----------------
 
 def create_thread(target):
     thread = threading.Thread(target=target)
@@ -22,9 +22,6 @@ import socket
 
 HOST = '127.0.0.1'
 PORT = 62107
-connection_established = False
-conn, addr = None, None # Define connection and address
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen(1)
@@ -35,84 +32,96 @@ print('Client connected by ', str(addr))
 #------------------------- Recieve data from client --------------------------------
 
 def recieve_data():
-    global x2,y2
+    global x2,y2                                 # Global x2, y2 to change position of player 2
     while True:
         data = conn.recv(1024).decode()
         data = data.split('-')
-        #print(data)
-        x2, y2 = float(data[0]), float(data[1])
+        x2, y2 = float(data[0]), float(data[1])  # Change value of position player 2
 
 #------------------------ Another thread doing about server ------------------------------
 
 create_thread(recieve_data)
 
 #------------------------- Create a display of game ---------------------------------
-size_game = (800,600)
+
+size_game = (g_width,g_height) = (800,600)
 surface = pygame.display.set_mode(size_game)
-pygame.display.set_caption("Ball") #caption
-# icon = pygame.image.load(os.path.join('games.png'))
-# pygame.display.set_icon (icon)
+pygame.display.set_caption("Pikachu Volley") 
+icon = pygame.image.load(os.path.join('src','icon.png'))
+pygame.display.set_icon (icon)
 clock = pygame.time.Clock() 
 
-x2, y2 = 675, 490
+#------------------------ Declare values before recieve from client ----------------
+
+x2, y2 = 675, 490           # Declare position of player 2 before recieve position from client
+
+
+#----------------------------------- Main -------------------------------------------
 
 def start_the_game():
-    # Main game
-
     running = True
-    x1, y1 = 375, 490
 
-    move = 20
-    jump = False
-    jump_count = 10
-    grav = 0.6
+    x1, y1 = 50, 490  # Initial position of player 1
 
+    # Detail of player 
+    move = 20               # Speed of player 
+    jump = False            # To check player jumping?
+    jump_count = 10         # State of jumping
+    grav = 0.5              # Gravity
 
-
+    # Import background in to game
     surface = pygame.display.set_mode(size_game)
-    bg = pygame.image.load("pikachu_background.png")
+    bg = pygame.image.load(os.path.join('src','pikachu_background.png'))
+
+    # Import image of player, ball
+    img_ball = pygame.image.load(os.path.join('src','ball.png'))
+    img_player1 = pygame.image.load(os.path.join('src','pika.png'))
+    img_player2 = pygame.image.load(os.path.join('src','pika2.png'))
+
+    # Detail of ball
+    block_size = 60         # Size of ball
+    x = 5                   # Initial speed of ball in x 
+    y = 0                   # Initial speed of ball in y
+    friction_x = 1          # Friction when ball bouncing
+    pos_x,pos_y  = 40, 40   # Initial position of ball
+    
+    score_player1 = 0       # Initial score of player 1
+    score_player2 = 0       # Initial score of player 2
 
 
-
-
-    block_size = 60
-    x = 5
-    y = 0
-    grav = 0.5
-    friction_x = 1
-    pos_x = 40
-    pos_y = 40
-
-
-
-
-
-
-
-
+    #------------------- Game when create server and client connected --------------
     while running:
 
-        #------------------------------------ Player ----------------------
+        # -------------- Condtion before get into the game ------------
+        keyspressed = pygame.key.get_pressed()
+        for event in pygame.event.get():            # User did something 
+            if event.type == pygame.QUIT:           # If user clicked exit
+                running = False                     # End the loop
+
+
+        # ------------------ Make a shape of player --------
         player1 = Rect(x1, y1, 50, 80)
         player2 = Rect(x2, y2, 50, 80)
 
 
+        # ---------------------- Input background ----------
         surface.blit(bg, (0, 0))
-        keyspressed = pygame.key.get_pressed()
-        for event in pygame.event.get(): # User did something
-            #print(event)    # Useful debugging tip    
-            if event.type == pygame.QUIT: # If user clicked close
-                running = False # Flag that we are done so we exit this loop
-        if keyspressed[ord("\x1b")]: # Pressing the x Key will quit the game
+
+
+        # -------------- Pressing the ESC Key to quit the game --------
+        if keyspressed[ord("\x1b")]: 
                 running = False             
-        if keyspressed[ord("a")]:
+
+
+        # ------- Moving player ----------
+        if keyspressed[ord("a")]:                   # Pressing A to move left
             x1 -= move
-        if keyspressed[ord("d")]:
+        if keyspressed[ord("d")]:                   # Pressing D to move right
             x1 += move
-        
-        if not(jump):
-            if keyspressed[ord("w")] or keyspressed[ord(" ")]:
-                jump = True
+        if not(jump):                               # Pressing W or Spacebar to jump
+            if keyspressed[ord("w")] \
+            or keyspressed[ord(" ")]:
+                jump = True                         # If jump = True we will calculate Gravity
         else:
             if jump_count >= -10:
                 neg = 1
@@ -124,84 +133,113 @@ def start_the_game():
                 jump = False
                 jump_count = 10
 
-                    #-------- Detect player won't out of size game -----------
+
+        #---------------- Detect player won't out of bound --------------
         if y1 < 0: 
             y1 = 0
         if y1 >= surface.get_height()-110: 
             y1 = surface.get_height()-110
         if x1 < 0:  
             x1 = 0
-        if x1 >= surface.get_width()-450: 
-            x1 = surface.get_width()-450   
+        if x1 >= surface.get_width()-475: 
+            x1 = surface.get_width()-475   
+
         
-
-
-
-
-
-
-        # --------------------- Ball -----------------------------
+        # --------------------- Making a ball -----------------------------
         ball = Rect(pos_x,pos_y,block_size,block_size)
+        
+        pos_x += x                              # Make the ball moving in x
+        pos_y += y                              # Make the ball moving in y
 
-        pos_x += x
-        pos_y += y
 
-        if pos_y + block_size > size_game[1] and y <= 2.0:
+        # ---------------------- The ball hit the wall --------------------
+        if pos_y + block_size > g_height and y <= 2.0: 
             x = 0
             y = 0
             grav = 0
             friction_x = 0
-        if pos_y + block_size > size_game[1] or pos_y < 20: 
+        
+        if pos_y < 0:                           # If the ball hit the top frame
             y = -y-grav
-            print(pos_y)
-        if pos_x + block_size > size_game[0] or pos_x < 20:
-            x = -(x*friction_x)
 
+            
+        if pos_x + block_size > g_width or \
+            pos_x < 0:                        # If the ball hit the left or right wall
+            x = -(x*friction_x)
+        
+        if pos_x + block_size == g_width // 2 and \
+           pos_y + block_size > g_height // 2:
+            x = -(x*friction_x)
+        if pos_x + block_size == g_width // 2 and \
+           pos_y + block_size == g_height // 2:
+            y = -y-grav
+        
+
+        # --------------------- Ball hit the player floor ------------------------
+        # when the player 2 win, the ball will release in player 2 side
+        if pos_y + block_size > g_height and \
+           pos_x + block_size <= g_width // 2:     
+            
+            # Reset detail of the ball and release  the ball in player 2 side
+            x, y = -5, 0                
+            friction_x = 1              
+            pos_x, pos_y = 700, 40
+
+            # Plus score to player 2 
+            score_player2 += 1
+            print("Player1 lose")
+        
+        # when the player 1 win, the ball will release in player 1 side
+        if pos_y + block_size > g_height and \
+           pos_x + block_size >= g_width // 2:     
+
+            # Reset detail of the ball and release the ball in player 1 side
+            x, y = 5, 0
+            friction_x = 1
+            pos_x, pos_y = 40, 40
+
+            # Plus score to player 1
+            score_player1 += 1
+            print("Player2 lose")
+        
 
         # ---------------- Ball hit player ------------------
-        
-        if ball.colliderect(player1) or ball.colliderect(player2):
-            x += 1
+        if ball.colliderect(player1) and y >= 0 or \
+           ball.colliderect(player2) and y >= 0:
             y = -y-grav
 
+
+        # ------------------------ Display Objective --------------------------
+        surface.blit(img_ball,ball.topleft)       # Display ball as Pokeball
+        y += grav                                 # When display the ball at first frame
+                                                  # then increase speed by gravity
         
+        surface.blit(img_player1,player1.topleft) # Display player1 as pikachu
+        surface.blit(img_player2,player2.topleft) # Display player2 as pikachu 
 
 
+        # ------------------------ Display score ------------------------------
+        font = pygame.font.Font(None, 80)
+        text_score_player1 = font.render(str(score_player1), False, (255, 0, 0))
+        text_score_player2 = font.render(str(score_player2), False, (255, 0, 0))      
+        surface.blit(text_score_player1, (60,60))
+        surface.blit(text_score_player2, (740,60))
 
 
+        # ------------------------ Send data to client ------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-        # ------------------------ DRAW --------------------------
-        pygame.draw.ellipse(surface, (0,0,0),ball)
-        y += grav
-
-        
-        pygame.draw.rect(surface, (204,0,255), player1)   
-
-        
-        pygame.draw.rect(surface, (0,0,255  ), player2)
-
-        send_data = '{}-{}-{}-{}'.format(x1, y1,pos_x,pos_y).encode()       # Use format string to enable encode function
+        send_data = '{}-{}-{}-{}-{}-{}'.format(x1, y1, pos_x, pos_y, score_player1, score_player2).encode()       # Use format string to enable encode function
         conn.send(send_data)
 
-        pygame.display.update()                         # Actually does the screen update
-        clock.tick(30)                                  # Run the game at 25 frames per second  
+
+        pygame.display.update()                    # Actually does the screen update
+        clock.tick(30)                             # Run the game at 30 frames per second  
         
+
+#------------------------ Menu before enter to the game --------------------------
 menu_widgth, menu_height = 400, 250
 menu = pygame_menu.Menu('Pikachu',menu_widgth,menu_height ,theme = pygame_menu.themes.THEME_DARK)
 menu.add.button('Play', start_the_game)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 menu.mainloop(surface)
 
-# Declare grid
-#grid = Grid()
